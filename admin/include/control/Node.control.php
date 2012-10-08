2587e22719a2ae37bf4fd04ff89b1f483d3faec9
luochuan wang <luochuan.wang@gmail.com>
2012-10-08 13:29:55 +0800

    123
    123

--- admin/include/control/Node.control.php
+++ admin/include/control/Node.control.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 /**
  * 节点管理模块
@@ -35,4 +36,43 @@ class NodeControl extends CommonControl
         $this->assign("nodeTree",$this->_node_model->getNodeTree());
         $this->display('node/add.html');
     }
+=======
+<?php
+/**
+ * 节点管理模块
+ * by wbq 2012-9-6
+ */
+class NodeControl extends CommonControl
+{
+    //组名称
+    private $_group = 'node';
+
+    private $_group_model = null;
+    private $_node_model = null;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        if (!$this->_group_model) $this->_group_model = N('Group');
+        if (!$this->_node_model) $this->_node_model = N('Node');
+    }
+
+    /**
+     * 主入口
+     */
+    public function index()
+    {
+        
+    }
+
+    /**
+     * 新节点入口
+     */
+    public function newNode()
+    {
+        $this->assign("nodeTree",$this->_node_model->getNodeTree());
+        $this->display('node/add.html');
+    }
+>>>>>>> parent of e0d681f... 1
 }
--- admin/include/model/Node.model.php
+++ admin/include/model/Node.model.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 /**
  * 节点模型 数据处理
@@ -17,4 +18,25 @@ class Node extends Base
 	{
 		return T('node')->select()->find(1);
 	}
+=======
+<?php
+/**
+ * 节点模型 数据处理
+ * by laucen 2012-9-6
+ */
+class Node extends Base
+{
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	/**
+	 * 获取节点树
+	 */
+	public function getNodeTree()
+	{
+		return T('node')->select()->find(1);
+	}
+>>>>>>> parent of e0d681f... 1
 }
