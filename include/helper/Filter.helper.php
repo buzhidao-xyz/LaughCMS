8d24aa3e5fd0a9a0747c40231d7c99aad7f01849
luochuan wang <luochuan.wang@gmail.com>
2012-10-09 17:39:25 +0800

    add company
    1

---
+++ include/helper/Filter.helper.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * filter类
+ * by laucen 2011-11-16
+ */
+ class FilterHelper
+ {
+    //控制器
+    protected $_helper = "Filter";
+
+    public function __construct()
+    {
+        
+    }
+
+    /**
+     * 检测是否数字
+     * @param $str int 字符
+     * @param $convert bool 是否转义 true转义 false不转义 默认false
+     */
+    static public function C_Numeric($str,$convert=false)
+    {
+    	$return = null;
+    	$return = is_numeric($str);
+
+    	return $return;
+    }
+ }
