17c0764572f293beb68d787ceeca070d5d8daa76
luochuan <luochuan.wang@gmail.com>
2012-10-23 22:41:26 +0800

    123

--- admin/cache/compile/cbc8f5b747cb8710ebc11a94ba2640fd59459142.file.add.html.php
+++ admin/cache/compile/cbc8f5b747cb8710ebc11a94ba2640fd59459142.file.add.html.php
@@ -1,17 +1,17 @@
-<?php /* Smarty version Smarty-3.1.12, created on 2012-10-23 17:57:30
+<?php /* Smarty version Smarty-3.1.11, created on 2012-10-22 17:27:12
          compiled from "C:\xampp\htdocs\laugh\admin\themes\default\Group\add.html" */ ?>
-<?php /*%%SmartyHeaderCode:894450866a0a4d6645-68070670%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+<?php /*%%SmartyHeaderCode:141285085116ff3bca1-76285195%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
   'file_dependency' => 
   array (
     'cbc8f5b747cb8710ebc11a94ba2640fd59459142' => 
     array (
       0 => 'C:\\xampp\\htdocs\\laugh\\admin\\themes\\default\\Group\\add.html',
-      1 => 1350984554,
+      1 => 1350897893,
       2 => 'file',
     ),
   ),
-  'nocache_hash' => '894450866a0a4d6645-68070670',
+  'nocache_hash' => '141285085116ff3bca1-76285195',
   'function' => 
   array (
   ),
@@ -20,10 +20,10 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     'perstatus' => 0,
   ),
   'has_nocache_code' => false,
-  'version' => 'Smarty-3.1.12',
-  'unifunc' => 'content_50866a0a520dd4_48986955',
+  'version' => 'Smarty-3.1.11',
+  'unifunc' => 'content_50851170046874_25717004',
 ),false); /*/%%SmartyHeaderCode%%*/?>
-<?php if ($_valid && !is_callable('content_50866a0a520dd4_48986955')) {function content_50866a0a520dd4_48986955($_smarty_tpl) {?><?php echo $_smarty_tpl->getSubTemplate ("include/head.html", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, null, null, array(), 0);?>
+<?php if ($_valid && !is_callable('content_50851170046874_25717004')) {function content_50851170046874_25717004($_smarty_tpl) {?><?php echo $_smarty_tpl->getSubTemplate ("include/head.html", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, null, null, array(), 0);?>
 
 <div id="perstatus" pers="<?php echo $_smarty_tpl->tpl_vars['perstatus']->value;?>
 "></div>
@@ -40,7 +40,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
 				<input type="text" name="title" value="" class="input" style="width:150px;" />
 			</li>
 			<li class="formblock">
-				<input type="submit" name="subut" class="button" value="添加" />
+				<input type="submit" name="subut" class="button" value="完成提交" />
 			</li>
 		</ul>
 	</form>
