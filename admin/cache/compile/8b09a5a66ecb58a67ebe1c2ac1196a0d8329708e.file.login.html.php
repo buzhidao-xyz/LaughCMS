a8c1bfbcefeb6887a4b0d79b79285258b48d1b23
luochuan wang <luochuan.wang@gmail.com>
2012-12-12 17:31:21 +0800

    123
    123

--- admin/cache/compile/8b09a5a66ecb58a67ebe1c2ac1196a0d8329708e.file.login.html.php
+++ admin/cache/compile/8b09a5a66ecb58a67ebe1c2ac1196a0d8329708e.file.login.html.php
@@ -1,6 +1,6 @@
-<?php /* Smarty version Smarty-3.1.12, created on 2012-12-12 17:19:51
+<?php /* Smarty version Smarty-3.1.12, created on 2012-12-12 17:28:19
          compiled from "C:\xampp\htdocs\laugh\admin\themes\blue\login.html" */ ?>
-<?php /*%%SmartyHeaderCode:2774350c84c37c95296-22463505%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+<?php /*%%SmartyHeaderCode:650050c84e335626f1-88400010%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
   'file_dependency' => 
   array (
@@ -11,7 +11,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
       2 => 'file',
     ),
   ),
-  'nocache_hash' => '2774350c84c37c95296-22463505',
+  'nocache_hash' => '650050c84e335626f1-88400010',
   'function' => 
   array (
   ),
@@ -21,9 +21,9 @@ $_valid = $_smarty_tpl->decodeProperties(array (
   ),
   'has_nocache_code' => false,
   'version' => 'Smarty-3.1.12',
-  'unifunc' => 'content_50c84c37cf01a9_00933829',
+  'unifunc' => 'content_50c84e335ba7e8_48088099',
 ),false); /*/%%SmartyHeaderCode%%*/?>
-<?php if ($_valid && !is_callable('content_50c84c37cf01a9_00933829')) {function content_50c84c37cf01a9_00933829($_smarty_tpl) {?><?php echo $_smarty_tpl->getSubTemplate ("include/head.html", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, null, null, array(), 0);?>
+<?php if ($_valid && !is_callable('content_50c84e335ba7e8_48088099')) {function content_50c84e335ba7e8_48088099($_smarty_tpl) {?><?php echo $_smarty_tpl->getSubTemplate ("include/head.html", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, null, null, array(), 0);?>
 
 <link type="text/css" rel="stylesheet" href="themes/blue/style/login.css" media="screen">
 <div id="loginTop"></div>
